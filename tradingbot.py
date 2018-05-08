ca438b85db0bd6e6f8037c43f23e5a42d316a628
GiantTree <marv.1997@live.de>
2018-05-08 21:20:04 +0200

    remove testing environment

--- tradingbot.py
+++ tradingbot.py
@@ -2,9 +2,6 @@ from telethon import TelegramClient
 
 import private
 
-testing = True
-
-client = TelegramClient(None if testing else "session1", private.api_id, private.api_hash)
-client.session.set_dc(2, "149.154.167.40", 80)
+client = TelegramClient("session1", private.api_id, private.api_hash)
 client.start()
 print(client.get_me())
