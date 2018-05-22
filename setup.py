52f931c8747e2f7bc489cece3247be90dc3129a5
GiantTree <marv.1997@live.de>
2018-05-22 20:58:07 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='TradingTipsClient',
+    version='0.1',
+    packages=[],
+    install_requires=[
+        'Telethon>=0.19',
+        'pyaes>=1.6.1',
+        'pyasn1>=0.4.2',
+        'rsa>=3.4.2'
+    ],
+    url='https://github.com/GiantTreeLP/TradingTipsParser',
+    license='GPL-3.0',
+    author='GiantTree',
+    author_email='gianttree+ttc@groundmc.net',
+    description=''
+)
